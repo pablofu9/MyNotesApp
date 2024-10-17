8026e1aa97312c405f93656e603596b755e3e682
Pablo Fuertes <pfuertes@fiveflamesmobile.com>
2024-10-17 09:27:47 +0200

    Initial Commit

---
+++ MyNotesApp/MyNotesAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyNotesAppApp.swift
+//  MyNotesApp
+//
+//  Created by Pablo Fuertes on 17/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyNotesAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
